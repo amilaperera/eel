9e81bdba9caffd1462e6b23562c4396c48f1e4c4
Amila Perera <perera.amila@gmail.com>
2018-09-27 17:04:25 +1000

    extending stm32 hal find package

--- src/cmake/FindSTM32HAL.cmake
+++ src/cmake/FindSTM32HAL.cmake
@@ -11,7 +11,12 @@ endif()
 if(NOT STM32HAL_FIND_COMPONENTS)
     set(STM32HAL_FIND_COMPONENTS ${HAL_COMPONENTS})
     message(STATUS "No STM32HAL components selected, using all: ${STM32HAL_FIND_COMPONENTS}")
+else()
+    message(STATUS "STM32HAL components: ${STM32HAL_FIND_COMPONENTS}")
 endif()
+
+set(HAL_HEADERS ${HAL_PREFIX}hal.h ${HAL_PREFIX}hal_def.h)
+set(HAL_SOURCES ${HAL_PREFIX}hal.c)
 
 foreach(cmp ${HAL_REQUIRED_COMPONENTS})
     # append the required components to the components list
@@ -20,4 +25,17 @@ foreach(cmp ${HAL_REQUIRED_COMPONENTS})
 endforeach()
 
 foreach(cmp ${STM32HAL_FIND_COMPONENTS})
+    list(APPEND HAL_HEADERS ${HAL_PREFIX}hal_${cmp}.h)
+    list(APPEND HAL_SOURCES ${HAL_PREFIX}hal_${cmp}.c)
+    list(APPEND HAL_HEADERS ${HAL_PREFIX}hal_${cmp}_ex.h)
+    list(APPEND HAL_SOURCES ${HAL_PREFIX}hal_${cmp}_ex.c)
 endforeach()
+
+list(REMOVE DUPLICATES HAL_HEADERS)
+list(REMOVE DUPLICATES HAL_SOURCES)
+
+find_path(STM32HAL_INCLUDE_DIR
+    ${HAL_HEADERS}
+    PATH_SUFFIXES stm32/${STM32_FAMILY_LOWER}
+    HINTS 
+    )
