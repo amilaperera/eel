016903f8188fd038bbf044688f48d1c0aed67f47
Amila Perera <perera.amila@gmail.com>
2018-09-28 22:45:00 +1000

    stm32 hal for F1

--- src/cmake/FindSTM32HAL.cmake
+++ src/cmake/FindSTM32HAL.cmake
@@ -1,10 +1,12 @@
 if(STM32_FAMILY_UPPER STREQUAL "F1")
-    set(HAL_COMPONENTS
-        gpio)
     set(HAL_REQUIRED_COMPONENTS
         cortex pwr rcc)
-    set(HAL_EX_COMPONENTS
+    set(HAL_COMPONENTS
         gpio)
+    set(HAL_EX_COMPONENTS
+        rcc
+        gpio)
+    # common prefix for f1
     set(HAL_PREFIX stm32f1xx_)
 endif()
 
@@ -19,23 +21,35 @@ set(HAL_HEADERS ${HAL_PREFIX}hal.h ${HAL_PREFIX}hal_def.h)
 set(HAL_SOURCES ${HAL_PREFIX}hal.c)
 
 foreach(cmp ${HAL_REQUIRED_COMPONENTS})
-    # append the required components to the components list
     list(APPEND STM32HAL_FIND_COMPONENTS ${cmp})
-    # TODO: Remove duplicates
 endforeach()
 
 foreach(cmp ${STM32HAL_FIND_COMPONENTS})
     list(APPEND HAL_HEADERS ${HAL_PREFIX}hal_${cmp}.h)
     list(APPEND HAL_SOURCES ${HAL_PREFIX}hal_${cmp}.c)
-    list(APPEND HAL_HEADERS ${HAL_PREFIX}hal_${cmp}_ex.h)
-    list(APPEND HAL_SOURCES ${HAL_PREFIX}hal_${cmp}_ex.c)
+    # add ex component
+    list (FIND HAL_EX_COMPONENTS ${cmp} FOUND_INDEX)
+    if(NOT ${FOUND_INDEX} LESS 0)
+      list(APPEND HAL_HEADERS ${HAL_PREFIX}hal_${cmp}_ex.h)
+      list(APPEND HAL_SOURCES ${HAL_PREFIX}hal_${cmp}_ex.c)
+    endif()
 endforeach()
 
-list(REMOVE DUPLICATES HAL_HEADERS)
-list(REMOVE DUPLICATES HAL_SOURCES)
+# Now remove all the duplicates
+list(REMOVE_DUPLICATES HAL_HEADERS)
+list(REMOVE_DUPLICATES HAL_SOURCES)
 
-find_path(STM32HAL_INCLUDE_DIR
-    ${HAL_HEADERS}
-    PATH_SUFFIXES stm32/${STM32_FAMILY_LOWER}
-    HINTS 
-    )
+# export include directory
+set(STM32HAL_INCLUDE_DIR ${CMAKE_CURRENT_LIST_DIR}/../platform/stm32/${STM32_FAMILY_LOWER}/STM32${STM32_FAMILY_UPPER}xx_HAL_Driver/Inc)
+
+# export all the sources
+foreach(SRC ${HAL_SOURCES})
+  find_file(FILE_${SRC} ${SRC}
+    PATH_SUFFIXES Src
+    PATHS ${CMAKE_CURRENT_LIST_DIR}/../platform/stm32/${STM32_FAMILY_LOWER}/STM32${STM32_FAMILY_UPPER}xx_HAL_Driver)
+  list(APPEND STM32HAL_SOURCES ${FILE_${SRC}})
+endforeach ()
+
+include(FindPackageHandleStandardArgs)
+find_package_handle_standard_args(STM32HAL DEFAULT_MSG STM32HAL_INCLUDE_DIR STM32HAL_SOURCES)
+
