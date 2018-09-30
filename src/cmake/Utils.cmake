dc6af0e2c435df8489f18456f98cada3aa6a5d5c
Amila Perera <perera.amila@gmail.com>
2018-09-30 13:30:34 +1000

    adding post build command

--- src/cmake/Utils.cmake
+++ src/cmake/Utils.cmake
@@ -14,3 +14,16 @@ function(SetTargetProperties Target)
     target_compile_options(${Target} PUBLIC -D${TARGET_DEFS})
   endif()
 endfunction()
+
+macro(ADD_POST_BUILD_COPY BINARY)
+  set(HEX_FILE ${BINARY}.hex)
+  set(BIN_FILE ${BINARY}.bin)
+  add_custom_command(TARGET ${BINARY}.elf
+    POST_BUILD
+    COMMAND ${CMAKE_OBJCOPY} -Oihex ${BINARY}.elf ${HEX_FILE}
+    COMMAND ${CMAKE_OBJCOPY} -Obinary ${BINARY}.elf ${BIN_FILE}
+    COMMENT "\nBuilding ${HEX_FILE} \nBuilding ${BIN_FILE}\n")
+  unset(HEX_FILE)
+  unset(BIN_FILE)
+endmacro()
+
--- src/samples/boards/stm32f103rb-nucleo/blinky/main.cpp
+++ src/samples/boards/stm32f103rb-nucleo/blinky/main.cpp
@@ -1,4 +1,22 @@
+#include "stm32f1xx_hal.h"
+
+#define ld2_Pin GPIO_PIN_5
+#define ld2_GPIO_Port GPIOA
+
 int main() {
-  while (1) {
-  }
+  HAL_Init();
+
+  GPIO_InitTypeDef GPIO_InitStruct;
+
+  __HAL_RCC_GPIOA_CLK_ENABLE();
+
+  HAL_GPIO_WritePin(ld2_GPIO_Port, ld2_Pin, GPIO_PIN_RESET);
+
+  GPIO_InitStruct.Pin = ld2_Pin;
+  GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
+  GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
+  HAL_GPIO_Init(ld2_GPIO_Port, &GPIO_InitStruct);
+
+  HAL_GPIO_WritePin(ld2_GPIO_Port, ld2_Pin, GPIO_PIN_SET);
+  while (1) { }
 }
