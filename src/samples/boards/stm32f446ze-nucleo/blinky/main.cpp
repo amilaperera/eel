fbf8746e31e9751d161f9cc14106f7b5eac42ba5
Amila Perera <perera.amila@gmail.com>
2019-02-02 00:13:33 +1100

    further slight refactoring

--- src/samples/boards/stm32f446ze-nucleo/blinky/main.cpp
+++ src/samples/boards/stm32f446ze-nucleo/blinky/main.cpp
@@ -1,14 +1,20 @@
-#include "hal/critical_section.hh"
+#include "stm32f4xx_hal.h" // HAL_Init() & CLK_ENABLE dependencies
 #include "hal/gpio.hh"
+
+using eel::hal::gpio::Pin;
+using eel::hal::gpio::Mode;
+using eel::hal::gpio::PullUpDown;
+using eel::hal::gpio::OutputType;
+using eel::hal::gpio::OutputSpeed;
 
 int main() {
   HAL_Init();
 
   __GPIOB_CLK_ENABLE();
 
-  eel::hal::ll::gpio::Gpio pin{eel::hal::gpio::Pin::B7};
-  pin.SetMode(eel::hal::gpio::Mode::Output);
-  pin.ConfigureOutput(eel::hal::gpio::PullUpDown::None, eel::hal::gpio::OutputType::PushPull, eel::hal::gpio::OutputSpeed::High);
+  eel::hal::ll::gpio::Gpio pin{Pin::B7};
+  pin.SetMode(Mode::Output);
+  pin.ConfigureOutput(PullUpDown::None, OutputType::PushPull, OutputSpeed::High);
 
   for (;;) {
     pin.Write(true);
