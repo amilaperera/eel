ac20a5732d2d1d8f3c784dd0595ac1d7256a1574
Amila Perera <perera.amila@gmail.com>
2019-02-21 23:09:23 +1100

    usart stm32 example

--- src/samples/boards/stm32f446ze-nucleo/usart_stm32/main.cpp
+++ src/samples/boards/stm32f446ze-nucleo/usart_stm32/main.cpp
@@ -1,24 +1,46 @@
+#include <cstring>
 #include "stm32f4xx_hal.h"
+#include "stm32f4xx_hal_conf.h"
 
 int main() {
   HAL_Init();
 
-  __GPIOB_CLK_ENABLE();
-
+  // TX RX configuration
+  // On board -
+  // USART3
+  // Tx - PD8
+  // Rx - PD9
+  __GPIOD_CLK_ENABLE();
   GPIO_InitTypeDef GPIO_InitStructure;
 
-  GPIO_InitStructure.Pin = GPIO_PIN_7;
+  GPIO_InitStructure.Pin = GPIO_PIN_8 | GPIO_PIN_9;
+  GPIO_InitStructure.Mode = GPIO_MODE_AF_PP;
+  GPIO_InitStructure.Alternate = GPIO_AF7_USART3;
+  GPIO_InitStructure.Speed = GPIO_SPEED_MEDIUM;
+  GPIO_InitStructure.Pull = GPIO_PULLUP;
+  HAL_GPIO_Init(GPIOD, &GPIO_InitStructure);
 
-  GPIO_InitStructure.Mode = GPIO_MODE_OUTPUT_PP;
-  GPIO_InitStructure.Speed = GPIO_SPEED_HIGH;
-  GPIO_InitStructure.Pull = GPIO_NOPULL;
-  HAL_GPIO_Init(GPIOB, &GPIO_InitStructure);
+  // usart configuration
+  __HAL_RCC_USART3_CLK_ENABLE();
+  UART_HandleTypeDef usart;
+  usart.Instance = USART3;
+  usart.Init.BaudRate = 115200;
+  usart.Init.Parity = UART_PARITY_NONE;
+  usart.Init.WordLength = UART_WORDLENGTH_8B;
+  usart.Init.StopBits = UART_STOPBITS_1;
+  usart.Init.HwFlowCtl = UART_HWCONTROL_NONE;
+  usart.Init.Mode = UART_MODE_TX_RX;
+  usart.Init.OverSampling = UART_OVERSAMPLING_16;
 
+  if (HAL_UART_Init(&usart) != HAL_OK) {
+    asm("bkpt 255");
+  }
+  const char *p = "\r\nWelcome !!!\r\n";
+  HAL_UART_Transmit(&usart, (uint8_t *)(p), std::strlen(p), HAL_MAX_DELAY);
   for (;;) {
-    HAL_GPIO_WritePin(GPIOB, GPIO_PIN_7, GPIO_PIN_SET);
-    HAL_Delay(500);
-    HAL_GPIO_WritePin(GPIOB, GPIO_PIN_7, GPIO_PIN_RESET);
-    HAL_Delay(500);
+    uint8_t ch;
+    HAL_UART_Receive(&usart, &ch, 1, HAL_MAX_DELAY);
+    HAL_UART_Transmit(&usart, &ch, 1, HAL_MAX_DELAY);
   }
 }
 
