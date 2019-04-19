63fadee27d7ed23655fa9caa05d87b9d03c102f6
Amila Perera <perera.amila@gmail.com>
2019-04-19 21:50:10 +1000

    exti enable & disable

--- src/platform/stm32/ll/exti/exti.cc
+++ src/platform/stm32/ll/exti/exti.cc
@@ -7,11 +7,12 @@
 
 using eel::util::set_bit;
 using eel::util::clear_bit;
+using eel::util::U32;
 
 namespace eel::hal::ll {
 
 void Exti::set_trigger(ExtiLine line, Trigger trigger) {
-  auto n{static_cast<eel::util::U32>(line)};
+  auto n{static_cast<U32>(line)};
   switch (trigger) {
     case Trigger::kFalling:
       exti_reg()->FTSR = set_bit(exti_reg()->FTSR, n);
@@ -29,11 +30,17 @@ void Exti::set_trigger(ExtiLine line, Trigger trigger) {
 }
 
 void Exti::enable(ExtiLine line) {
-
+  // enable interrupt
+  exti_reg()->IMR = set_bit(exti_reg()->IMR, static_cast<U32>(line));
+  // enable event
+  exti_reg()->EMR = set_bit(exti_reg()->EMR, static_cast<U32>(line));
 }
 
 void Exti::disable(ExtiLine line) {
-
+  // disable interrupt
+  exti_reg()->IMR = clear_bit(exti_reg()->IMR, static_cast<U32>(line));
+  // disable event
+  exti_reg()->EMR = clear_bit(exti_reg()->EMR, static_cast<U32>(line));
 }
 
 }
